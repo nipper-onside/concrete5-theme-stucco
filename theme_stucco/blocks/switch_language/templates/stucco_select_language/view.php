d9bfad3532c0ea2e9e3ffd27ae591ff6f9a56576
nipper-onside <nipper.onside@gmail.com>
2017-04-14 16:48:18 +0900

    Fix for Custom Template of Switch Language Block

--- theme_stucco/blocks/switch_language/templates/stucco_select_language/view.php
+++ theme_stucco/blocks/switch_language/templates/stucco_select_language/view.php
@@ -2,15 +2,15 @@
 $ih = Core::make('multilingual/interface/flag');
 ?>
 <div class="stucco-block-switch-language-flags">
-	<div class="stucco-block-switch-language-flags-label"><?php  echo $label?></div>
+	<div class="stucco-block-switch-language-flags-label"><?php echo $label?></div>
 	<ul>
-	<?php  foreach($languageSections as $ml) : ?>
+	<?php foreach($languageSections as $ml) : ?>
 		<li><a href="<?php  echo $view->action('switch_language', $cID, $ml->getCollectionID())?>" title="<?php  echo $ml->getLanguageText($locale)?>" class="<?php  if ($activeLanguage == $ml->getCollectionID()) { ?>stucco-block-switch-language-active-flag<?php  } ?>">
-		<?php  echo $ml->msLanguage ?>
+		<?php echo $ml->getLanguage() ?>
 		</a></li>
 
 <?php  //var_dump($ml) ?>
 
-	<?php  endforeach; ?>
+	<?php endforeach; ?>
 	</ul>
 </div>
