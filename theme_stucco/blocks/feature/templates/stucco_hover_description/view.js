7b64e865ae32c080ca1cfac7550fd52768cc4003
nipper-onside <nipper.onside@gmail.com>
2017-04-14 16:45:30 +0900

    Fix for Custom Template of Feature Block

--- theme_stucco/blocks/feature/templates/stucco_hover_description/view.js
+++ theme_stucco/blocks/feature/templates/stucco_hover_description/view.js
@@ -2,7 +2,9 @@ $(function() {
     $('.ccm-block-feature-item-hover-wrapper').tooltip();
     $('.ccm-block-feature-item-hover-wrapper').on('shown.bs.tooltip', function () {
         $('.tooltip').addClass('animated bounceInDown');
+        $(this).find('.ccm-block-feature-item-hover-icon').addClass('hover');
     }).on('hide.bs.tooltip', function () {
         $('.tooltip').removeClass('animated bounceInDown');
+         $('.ccm-block-feature-item-hover-icon').removeClass('hover');
     });
 });
