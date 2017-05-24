7770c2f79b438b36939878a1c341ab1a41c1299f
nipper-onside <nipper.onside@gmail.com>
2017-05-24 18:37:07 +0900

    Fix for Custom Template of Feature Block

--- theme_stucco/blocks/feature/templates/stucco_card_description/view.php
+++ theme_stucco/blocks/feature/templates/stucco_card_description/view.php
@@ -1,22 +1,34 @@
 <?php   defined('C5_EXECUTE') or die("Access Denied."); ?>
-<?php 
-$title = h($title);
+<?php
+//$title = h($title);
+/*
 if ($linkURL) {
     $title = '<a href="' . $linkURL . '">' . $title . '</a>';
 }
+*/
 ?>
 <div class="stucco-card-feature-item">
+<?php  if ($linkURL) { ?>
+    <a href="<?php  echo $linkURL?>">
+<?php  } else {?>
+	<span>
+<?php  } ?>
     <?php  if ($title) { ?>
-        <h4><?php  echo $title?></h4>
+        <h4><?php  echo h($title)?></h4>
     <?php  } ?>
     	<div class="icon">
     		<i class="fa fa-<?php  echo $icon?>"></i>
     	</div>
     	<div class="card-body">
-		<?php 
+		<?php
 		if ($paragraph) {
 			echo $paragraph;
 		}
 	    ?>
 	    </div>
+<?php  if ($linkURL) { ?>
+    </a>
+<?php  } else {?>
+	</span>
+<?php  } ?>
 </div>
